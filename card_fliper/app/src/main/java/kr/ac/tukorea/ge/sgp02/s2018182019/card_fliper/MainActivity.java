e97c3dcbef443748251de3674912a5b37a7ffbd9
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-03-22 16:06:14 +0900

    각 버튼의 onClick 함수 추가

--- card_fliper/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/card_fliper/MainActivity.java
+++ card_fliper/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/card_fliper/MainActivity.java
@@ -3,12 +3,24 @@ package kr.ac.tukorea.ge.sgp02.s2018182019.card_fliper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+}
+
+public void onBtnCard(View view) {
+    Log.d(TAG,"card pressedd" + view.getId());
+}
+
+public void onBtnRestart(View view) {
+    Log.d(TAG,"restart pressed");
 }
