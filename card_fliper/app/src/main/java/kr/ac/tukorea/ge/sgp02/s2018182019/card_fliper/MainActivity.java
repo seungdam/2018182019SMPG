63515c5856853738d13c0fcba1539f5fe8e4befc
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-03-22 16:20:53 +0900

    카드 클릭시 index 출력하게 로그 작성

--- card_fliper/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/card_fliper/MainActivity.java
+++ card_fliper/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/card_fliper/MainActivity.java
@@ -9,7 +9,12 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
+    private static final int[] BUTTON_IDS = new int[] {
+            R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
+            R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
+            R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
+            R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +22,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnCard(View view) {
-        Log.d(TAG,"card pressedd" + view.getId());
+        int btnIndex = findButtonIndex(view.getId());
+        Log.d(TAG,"card pressedd" + btnIndex);
     }
+
+    private int findButtonIndex(int id) {
+        for(int i = 0; i < BUTTON_IDS.length; ++i) {
+            if(id == BUTTON_IDS[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public void onBtnRestart(View view) {
         Log.d(TAG,"restart pressed");
     }
