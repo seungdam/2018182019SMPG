b9c7583bb848fe2436ea76d1f362792e23b00588
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-04-04 22:40:36 +0900

    ball 객체를 배열을 통해 관리해보자

--- SampleGame/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/samplegame/GameView.java
+++ SampleGame/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/samplegame/GameView.java
@@ -14,14 +14,16 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 
 public class GameView extends View implements Choreographer.FrameCallback {
-    private Ball ball1,ball2;
+
+    private ArrayList<Ball> balls = new ArrayList<>();
 
     private long previousTimeNanos;
     private int framesPerSecond; // 전역 변수 선언시 항상 풀네임으로 작성하도록 한다.
     private Paint fpsTextPaint = new Paint();
-
     public static GameView view;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
@@ -31,35 +33,44 @@ public class GameView extends View implements Choreographer.FrameCallback {
 
     // 게임뷰 초기화시 단 한번만 수행하는 함수.
     private void initView() {
+
+
         // 공의 비트맵 불러오기
         Resources res = getResources();
         Bitmap soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
         Ball.setBitmap(soccerBitmap);
 
-
+        // 뷰에 대한 정보 담기
         view = this;
+
         // 프레임 출력 텍스트 컨텐츠
         fpsTextPaint.setColor(Color.GREEN);
         fpsTextPaint.setTextSize(100);
 
+        Ball ball1,ball2;
+
         ball1 = new Ball(10,10);
         ball2 = new Ball(7, 15);
 
+        balls.add(ball1);
+        balls.add(ball2);
         Choreographer.getInstance().postFrameCallback(this);
     }
     // 축구공을 움직여 보자
 
 
     private void update() {
-        ball1.update();
-        ball2.update();
+       for(Ball ball:balls) {
+           ball.update();
+       }
     }
 
     // 모든 그리기 작업
     @Override
     protected void onDraw(Canvas canvas) {
-        ball1.render(canvas);
-        ball2.render(canvas);
+        for(Ball ball:balls) {
+            ball.render(canvas);
+        }
         canvas.drawText("FPS: " + framesPerSecond, framesPerSecond * 10, 100, fpsTextPaint);
     }
 
