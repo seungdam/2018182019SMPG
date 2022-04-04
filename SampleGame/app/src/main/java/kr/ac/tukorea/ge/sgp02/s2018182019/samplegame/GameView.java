d29332ea81e908ca65759969f3501c4c44851dea
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-04-04 21:33:24 +0900

    게임뷰에서 축구공 띄우기

--- SampleGame/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/samplegame/GameView.java
+++ SampleGame/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/samplegame/GameView.java
@@ -1,28 +1,37 @@
 package kr.ac.tukorea.ge.sgp02.s2018182019.samplegame;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private Bitmap soccerBitmap;
+    private Rect soccerSrcRect = new Rect();
+    private Rect soccerDstRect = new Rect();
 
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         initView();
     }
-    
+
     // 게임뷰 초기화시 단 한번만 수행하는 함수.
     private void initView() {
-
+        Resources res = getResources();
+        soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+        soccerSrcRect.set(0,0,soccerBitmap.getWidth(),soccerBitmap.getHeight());
+        soccerDstRect.set(0,0,200,200);
 
     }
-
     @Override
     protected void onDraw(Canvas canvas) {
-
+        canvas.drawBitmap(soccerBitmap,soccerSrcRect,soccerDstRect,null);
     }
 }
