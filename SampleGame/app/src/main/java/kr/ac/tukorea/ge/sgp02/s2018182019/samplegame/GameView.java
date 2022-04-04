883c681648eddebbd7eb4565d62babca5ff122b6
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-04-04 22:10:53 +0900

    choreography를 이용한 프레임

--- SampleGame/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/samplegame/GameView.java
+++ SampleGame/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/samplegame/GameView.java
@@ -5,20 +5,25 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.Choreographer;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 
-public class GameView extends View {
+public class GameView extends View implements Choreographer.FrameCallback {
     private Bitmap soccerBitmap;
     private Rect soccerSrcRect = new Rect();
     private Rect soccerDstRect = new Rect();
     int ballDX, ballDY; // 공들의 이동변화량 = 속도
-
+    private long previousTimeMillis;
+    private int framesPerSecond; // 전역 변수 선언시 항상 풀네임으로 작성하도록 한다.
+    private Paint fpsTextPaint = new Paint();
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -27,31 +32,23 @@ public class GameView extends View {
 
     // 게임뷰 초기화시 단 한번만 수행하는 함수.
     private void initView() {
+        // 공의 비트맵 불러오기
         Resources res = getResources();
         soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
         soccerSrcRect.set(0,0,soccerBitmap.getWidth(),soccerBitmap.getHeight());
         soccerDstRect.set(0,0,200,200);
 
+        // 프레임 출력 텍스트 컨텐츠
+        fpsTextPaint.setColor(Color.GREEN);
+        fpsTextPaint.setTextSize(100);
+
+
         ballDX = 10;
         ballDY = 10;
-
-        updateFrame();
-
+        Choreographer.getInstance().postFrameCallback(this);
     }
     // 축구공을 움직여 보자
-    private void updateFrame() {
-        update();
-        // 화면을 갱신 시키는 함수. 생성자 호출 제외
-        invalidate();
 
-        // 핸들러 객체를 따로 생성하지 않고 뷰가 가지는 핸들러로 이를 수행하자.
-        post(new Runnable() { // 메세지를 보낸다고 해석하면된다.
-            @Override
-            public void run() {
-                updateFrame(); // updateFrame이라는 기능을 수행하라고 메세지를 보낸다.
-            }
-        });
-    }
 
     private void update() {
         soccerDstRect.offset(ballDX,ballDY);
@@ -80,5 +77,19 @@ public class GameView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(soccerBitmap,soccerSrcRect,soccerDstRect,null);
+        canvas.drawText("FPS: " + framesPerSecond, framesPerSecond * 10, 100, fpsTextPaint);
+    }
+
+    @Override
+    public void doFrame(long l) {
+        
+            long now = System.currentTimeMillis();
+            int elapsed = (int) (now - previousTimeMillis);
+            framesPerSecond = 1000 / elapsed;
+            //Log.v(TAG, "Elapsed: " + elapsed + " FPS: " + framesPerSecond);
+            previousTimeMillis = now;
+            update();
+            invalidate();
+            Choreographer.getInstance().postFrameCallback(this);
     }
 }
